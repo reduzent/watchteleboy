bd7aae78a8dca94a51c4aef5762c8b75265ebc07
Roman Haefeli <reduzent@gmail.com>
2012-03-30 15:11:37 +0200

    update checksum in PKGBUILD file

--- arch/create-arch-package.sh
+++ arch/create-arch-package.sh
@@ -13,5 +13,5 @@ tar czf $ARCHIVENAME -C ../ \
 
 # update version in PKGBUILD
 sed -i -e "s/^pkgver=.*$/pkgver=${PROGVERSION}/" PKGBUILD
-
+sed -i -e "s/^md5sums=('.*')$/md5sums=('$(md5sum ${ARCHIVENAME} | cut -f 1 -d " ")')/" PKGBUILD
 exit 0
