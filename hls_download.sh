19cc4f4acd2e976543bd5b08ecc19a4af3861d72
Roman Haefeli <roman.haefeli@zhdk.ch>
2016-05-04 08:58:10 +0200

    hls download basically working at fixed bitrate (download might be slower than real time on slow connection)

--- hls_download.sh
+++ hls_download.sh
@@ -2,14 +2,29 @@
 stream_url="$1"
 stream_url_base="$(echo "$stream_url"  | \
   cut -d'/' -f-9)"
-counter=$(wget -O - -q "$stream_url" | \
+stream_start_segment=$(wget -O - -q "$stream_url" | \
   grep "#EXT-X-MEDIA-SEQUENCE" | \
   cut -d':' -f2
   )
-stream_start_time=$(date +%s)
-while wget "${stream_url_base}/${counter}.ts"
+stream_ref_segment=$((stream_start_segment+3))
+stream_current_segment=$stream_start_segment
+stream_ref_time=$(date +%s)
+while wget "${stream_url_base}/${stream_current_segment}.ts"
 do
-  cat ${counter}.ts >> stream.m2t
-  rm ${counter}.ts
-  ((counter+=1))
+  time_current=$(date +%s)
+  time_delta=$((time_current-stream_ref_time))
+  echo "time_delta: $time_delta"
+  segment_delta=$((stream_current_segment-stream_ref_segment))
+  echo "segment_delta: $segment_delta"
+  # set 0 if negative
+  segment_delta=$(( segment_delta > 0 ? segment_delta : 0))
+  echo "segment_delta: $segment_delta"
+  seconds_to_wait=$(( 2 + 4 * segment_delta - time_delta))
+  seconds_to_wait=$(( seconds_to_wait > 0 ? seconds_to_wait : 0))
+  stream_current_file=${stream_current_segment}.ts
+  cat $stream_current_file >> stream.m2t
+  rm $stream_current_file
+  echo "==== WAIT $seconds_to_wait SECONDS ======="
+  sleep "$seconds_to_wait"
+  ((stream_current_segment+=1))
 done
