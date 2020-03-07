05be6c61b824fc04d52d02dd48d07f9884e1d2b9
Roman Haefeli <reduzent@gmail.com>
2020-03-07 19:04:03 +0100

    make wt.__init__ more pythonic

--- wt/__init__.py
+++ wt/__init__.py
@@ -1,3 +1,24 @@
-from wt.classes import *
-from wt.gui import *
-from wt.helpers import *
+"""
+wt is the module used by the watchteleboy command line utility
+and comes with helper functions, classes for dealing with teleboy API
+and MPEG-DASH streams. wt.gui gives watchteleboy a nice urwid based
+frontend.
+"""
+
+from wt.classes import (WatchTeleboyError,
+                        WatchTeleboySession,
+                        WatchTeleboyStreamContainer,
+                        WatchTeleboyStreamHandler,
+                        WatchTeleboyPlayer)
+
+from wt.gui import (WatchTeleboyGUI,
+                    convert_mpv_timestring)
+
+from wt.helpers import (parse_args,
+                        parse_time_string,
+                        parse_duration_string,
+                        create_env,
+                        create_config,
+                        read_config,
+                        schedule_recording,
+                        delete_cronjob)
